123c7940f9fb1832e3ae988e20150da0bcede44a
cbitze7387k <Chad_Bitzer@cable.comcast.com>
2020-07-08 09:17:10 -0700

    fix < symbol in if statement and correct strconv.FormatFloat() fmt & prec, and delete testing lines)

--- cmd/fizzbuzz.go
+++ cmd/fizzbuzz.go
@@ -11,22 +11,18 @@ var tres float64
 var cinco float64
 
 func main() {
-	for i = 0; i >= 100; i++ {
+	for i = 0; i <= 100; i++ {
 		tres = math.Mod(i, 3)
 		cinco = math.Mod(i, 5)
 
-		strTres := strconv.FormatFloat(tres, 'E', 2, 64)
-		fmt.Println(strTres)
-		strCinco := strconv.FormatFloat(cinco, 'E', 2, 64)
-		fmt.Println(strCinco)
-		//		if tres < 1 && cinco < 1 {
-		//			fmt.Sprintln(i)
-		//		} else if tres == 0 && cinco < 1 {
-		//			fmt.Println("Fizz")
-		//		} else if tres < 1 && cinco == 0 {
-		//			fmt.Println("Buzz")
-		//		} else {
-		//			fmt.Println("FizzBuzz")
-		//		}
+		if tres != 0 && cinco != 0 {
+			fmt.Println(strconv.FormatFloat(i, 'f', 0, 64))
+		} else if tres == 0 && cinco != 0 {
+			fmt.Println("Fizz")
+		} else if tres != 0 && cinco == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println("FizzBuzz")
+		}
 	}
 }
